f4cc43a45afc4a0f5a3d5fbe415e8c49767bb37d
github-action-benchmark <github@users.noreply.github.com>
2023-06-29 09:41:28 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for c16442d286ee9fd627a84512ef83aed27ffa9263

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1688031522756,
+  "lastUpdate": 1688031688290,
   "repoUrl": "https://github.com/willGraham01/benchmark-action-playtest",
   "entries": {
     "Benchmark": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 0.007042,
             "unit": "Seconds",
             "extra": "(Profile timestamp) 2023-06-29_09:38:42,\n(Writing) timestamp 2023-06-29_09:38:42"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "1willgraham@gmail.com",
+            "name": "willGraham01",
+            "username": "willGraham01"
+          },
+          "committer": {
+            "email": "1willgraham@gmail.com",
+            "name": "willGraham01",
+            "username": "willGraham01"
+          },
+          "distinct": true,
+          "id": "c16442d286ee9fd627a84512ef83aed27ffa9263",
+          "message": "Deliberate slowdown introduced",
+          "timestamp": "2023-06-29T10:41:09+01:00",
+          "tree_id": "e4595d2795b2cbab79b9f90c1b3d632627ebb37f",
+          "url": "https://github.com/willGraham01/benchmark-action-playtest/commit/c16442d286ee9fd627a84512ef83aed27ffa9263"
+        },
+        "date": 1688031687819,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "benchmark.py: Runtime",
+            "value": 0.007339,
+            "unit": "Seconds",
+            "extra": "(Profile timestamp) 2023-06-29_09:41:27,\n(Writing) timestamp 2023-06-29_09:41:27"
           }
         ]
       }
