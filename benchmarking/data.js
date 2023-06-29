ad20aa2d257ef9de9c573f613e42e6f9dfd5dd82
github-action-benchmark <github@users.noreply.github.com>
2023-06-29 09:33:16 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for beb333ee83fad28d16f744ce10ffa47936786cc2

---
+++ benchmarking/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1688031196595,
+  "repoUrl": "https://github.com/willGraham01/benchmark-action-playtest",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "1willgraham@gmail.com",
+            "name": "willGraham01",
+            "username": "willGraham01"
+          },
+          "committer": {
+            "email": "1willgraham@gmail.com",
+            "name": "willGraham01",
+            "username": "willGraham01"
+          },
+          "distinct": true,
+          "id": "beb333ee83fad28d16f744ce10ffa47936786cc2",
+          "message": "Merge branch 'main' of github.com:willGraham01/benchmark-action-playtest",
+          "timestamp": "2023-06-29T10:32:55+01:00",
+          "tree_id": "42caf09e6f5ddec3dad09004ef6a701b17f9ed5e",
+          "url": "https://github.com/willGraham01/benchmark-action-playtest/commit/beb333ee83fad28d16f744ce10ffa47936786cc2"
+        },
+        "date": 1688031196179,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "benchmark.py: Runtime",
+            "value": 0.003481,
+            "unit": "Seconds",
+            "extra": "(Profile timestamp) 2023-06-29_09:33:15,\n(Writing) timestamp 2023-06-29_09:33:15"
+          }
+        ]
+      }
+    ]
+  }
+}
