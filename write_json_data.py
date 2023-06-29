c023465cec2a64ddd17d3ddbc6593b78a681e083
willGraham01 <1willgraham@gmail.com>
2023-06-29 09:33:03 +0100

    Rework script to give control over naming things

--- write_json_data.py
+++ write_json_data.py
@@ -8,13 +8,9 @@ LOCATION_OF_THIS_FILE = Path(os.path.abspath(os.path.dirname(__file__)))
 RECORD_DIR = (LOCATION_OF_THIS_FILE / "benchmark_records").resolve()
 
 
-def record_duration_to_json(session_file: Path, record_dir: Path = RECORD_DIR):
+def record_duration_to_json(session_file: Path, output_file: Path = None):
     # Grad current timestamp for tracking purposes
     time_now = datetime.utcnow().strftime("%Y-%m-%d_%H:%M:%S")
-
-    # Create output directory if it doesn't exist
-    if not os.path.exists(record_dir):
-        os.mkdir(record_dir)
 
     # Open the profiling session output
     with open(session_file, "r") as f:
@@ -25,19 +21,31 @@ def record_duration_to_json(session_file: Path, record_dir: Path = RECORD_DIR):
         raise RuntimeError(f"{session_file} does not record a start time!")
     else:
         timestamp = datetime.fromtimestamp(session_data["start_time"]).strftime(
-            "%Y-%m-%d %H:%M:%S"
+            "%Y-%m-%d_%H:%M:%S"
         )
+
+    # Prepare output file
+    if output_file is None:
+        output_file = RECORD_DIR / f"{timestamp}.json"
+    if (
+        not os.path.exists(os.path.dirname(output_file))
+        and os.path.dirname(output_file) != ""
+    ):
+        os.mkdir(os.path.dirname(output_file))
+    if output_file.suffix != ".json":
+        output_file = output_file.parent / f"{output_file.name}.json"
 
     # Prepare output file
     data_to_dump = dict()
     data_to_dump["name"] = session_data["program"] + ": Runtime"
     data_to_dump["unit"] = "Seconds"
     data_to_dump["value"] = session_data["cpu_time"]
-    data_to_dump["extra"] = f"(Writing) timestamp {time_now}"
+    data_to_dump[
+        "extra"
+    ] = f"(Profile timestamp) {timestamp},\n(Writing) timestamp {time_now}"
 
     # Write output
-    record_file = record_dir / (timestamp + ".json")
-    with open(record_file, "w", encoding="utf-8") as f:
+    with open(output_file, "w", encoding="utf-8") as f:
         json.dump(data_to_dump, f, ensure_ascii=True, indent=4)
     return
 
@@ -49,15 +57,15 @@ if __name__ == "__main__":
     parser.add_argument(
         "session_file",
         type=Path,
-        help="JSON file output of the profiling session",
+        help="JSON file output of the profiling session.",
     )
     parser.add_argument(
-        "record_dir",
+        "output_file",
         type=Path,
         nargs="?",
-        default=RECORD_DIR,
+        default=None,
         action="store",
-        help="Directory to save CPU duration to.",
+        help="Custom output file name to write result to.",
     )
 
     args = parser.parse_args()
