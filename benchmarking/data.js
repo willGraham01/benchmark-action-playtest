b39e13703a1631af6d62741cc790666818cafde8
github-action-benchmark <github@users.noreply.github.com>
2023-07-10 13:15:41 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for b215f7d886fff470544a9ddfe64c1c6b8fd22af7

--- benchmarking/data.js
+++ benchmarking/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1688994711230,
+  "lastUpdate": 1688994941620,
   "repoUrl": "https://github.com/willGraham01/benchmark-action-playtest",
   "entries": {
     "Benchmark": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 0.007592,
             "unit": "Seconds",
             "extra": "(Profile timestamp) 2023-07-10_13:11:50,\n(Writing) timestamp 2023-07-10_13:11:50"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "1willgraham@gmail.com",
+            "name": "willGraham01",
+            "username": "willGraham01"
+          },
+          "committer": {
+            "email": "1willgraham@gmail.com",
+            "name": "willGraham01",
+            "username": "willGraham01"
+          },
+          "distinct": true,
+          "id": "b215f7d886fff470544a9ddfe64c1c6b8fd22af7",
+          "message": "Try deploying to another place?",
+          "timestamp": "2023-07-10T14:15:16+01:00",
+          "tree_id": "5bf67567ca877b4ee13fa57d6369e3ee5f613bb2",
+          "url": "https://github.com/willGraham01/benchmark-action-playtest/commit/b215f7d886fff470544a9ddfe64c1c6b8fd22af7"
+        },
+        "date": 1688994940706,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "benchmark.py -m output_html/new_meta.json --html output_html/new.html: Runtime",
+            "value": 0.007902,
+            "unit": "Seconds",
+            "extra": "(Profile timestamp) 2023-07-10_13:15:40,\n(Writing) timestamp 2023-07-10_13:15:40"
           }
         ]
       }
